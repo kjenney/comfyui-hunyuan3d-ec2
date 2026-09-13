1735e6f187341927491729edf59e8a2583d69734
Ken Jenney <kjenney@gmail.com>
2026-09-13 12:13:15 -0400

    Use an ASG instead of a single instance

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,32 @@
+output "asg_name" {
+  description = "Name of the Auto Scaling Group"
+  value       = aws_autoscaling_group.comfyui.name
+}
+
 output "instance_id" {
   description = "EC2 instance ID"
-  value       = aws_instance.comfyui.id
+  value       = try(data.aws_instances.comfyui.ids[0], "")
 }
 
 output "public_ip" {
   description = "Public IP of the ComfyUI instance"
-  value       = aws_instance.comfyui.public_ip
+  value       = try(data.aws_instances.comfyui.public_ips[0], "")
 }
 
 output "private_ip" {
   description = "Private IP of the ComfyUI instance"
-  value       = aws_instance.comfyui.private_ip
+  value       = try(data.aws_instances.comfyui.private_ips[0], "")
 }
 
 output "comfyui_url" {
   description = "ComfyUI web interface URL"
-  value       = "http://${aws_instance.comfyui.public_ip}:${var.comfyui_port}"
+  value       = try("http://${data.aws_instances.comfyui.public_ips[0]}:${var.comfyui_port}", "")
 }
 
 output "ssh_command" {
   description = "SSH command to access the instance"
-  value       = var.key_name != "" ? "ssh -i ~/.ssh/${var.key_name}.pem ec2-user@${aws_instance.comfyui.public_ip}" : "ssh ec2-user@${aws_instance.comfyui.public_ip}"
+  value = try(
+    var.key_name != "" ? "ssh -i ~/.ssh/${var.key_name}.pem ec2-user@${data.aws_instances.comfyui.public_ips[0]}" : "ssh ec2-user@${data.aws_instances.comfyui.public_ips[0]}",
+    ""
+  )
 }
